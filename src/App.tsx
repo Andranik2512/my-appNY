53732eed5b4a23c69c8e3b360c46f9cb85266f13
Andranik <andranikaparan@gmail.com>
2026-03-20 18:23:44 -0400

    add urls

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-function App() {
+
+
+const Music = () => {
+  const tracks = [
+    {
+      title: 'music1',
+      url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+    },
+    {
+      title: 'music2',
+      url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
+    },
+    {
+      title: 'music2',
+      url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
+    },{
+      title: 'music2',
+      url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
+    },
+  ]
+
   return (
-    <h1>first commit</h1>
+    <div>
+      {tracks.map((track, index) => (
+        <div key={index}>
+          <p>{track.title}</p>
+          <audio controls src={track.url}></audio>
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default App
+export default Music
---
+++ src/music.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Music = () => {
+    const tracs = [
+        {title: 'music1', url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'},{title: 'music2', url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'}
+       ]
+  return (
+    
+    tracs
+  )
+}
+
+export default Music
